fdad0c1699ab9bdd509e559ead47df5a2c46e3ce
nerune-hub <tomatmh@gmail.com>
2021-04-02 12:52:25 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/weather-app/'   //任意
+//     : '/'
+// }
+
+// ページが真っ白なため上から変更
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/weather-app/'   //任意
-    : '/'
+    baseUrl: './'
 }
