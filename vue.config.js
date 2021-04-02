c2e7a9bb3f9c71b2d7554c96dd0f13b75b6e1597
nerune-hub <tomatmh@gmail.com>
2021-04-02 13:28:07 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
+// github pages用に追加
 module.exports = {
-  outputDir: './docs', //githubpages用 distの代わり
   publicPath: process.env.NODE_ENV === 'production'
     ? '/weather-app/'   //任意
     : '/'
 }
-
-// ページが真っ白なため上から変更
-// module.exports = {
-//     baseUrl: './',
-//     outputDir: 'docs/'
-// }
